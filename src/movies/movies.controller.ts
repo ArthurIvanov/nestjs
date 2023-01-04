0285f990124940725c2f7242cfd1e9d9629936fc
Artur Ivanov <artur.dsgn@yandex.ru>
2023-01-04 21:54:44 +0300

    added controllers and services for API

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { clearConfigCache } from 'prettier';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') year: string) {
+    return `We are searching for a movie with a year:  ${year}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') id: string, @Body() updateData) {
+    return this.moviesService.update(id, updateData);
+  }
+}
